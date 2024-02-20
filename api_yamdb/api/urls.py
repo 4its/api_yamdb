5a083dd1d3ab2f7de9a57535ad0c7e8fd8b47631
ARLIKIN <nik5552000@mail.ru>
2024-02-20 14:28:07 +0400

    Добавил модели для отзывов и коментариев так же сделал эндпоинты

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from .views import CategoryViewSet, TitleViewSet, GenresViewSet
+from .views import (
+    CategoryViewSet,
+    TitleViewSet,
+    GenresViewSet,
+    ReviewsViewSet,
+    CommentViewSet
+)
 
 router_v1 = SimpleRouter()
 router_v1.register(r'titles', TitleViewSet, basename='titles')
 router_v1.register(r'categories', CategoryViewSet, basename='categories')
 router_v1.register(r'genres', GenresViewSet, basename='genres')
+router_v1.register(r'reviews', ReviewsViewSet, basename='reviews')
+router_v1.register(r'comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
