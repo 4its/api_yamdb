2afd3ee0fc3fdc9ed6e5aa7de501ff7a3c66c9f5
Sergey Kulbida <kulbida.sergey@outlook.com>
2024-02-20 14:18:41 +0700

    Добавил модель для категорий

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.template.defaultfilters import truncatewords
 
-# Create your models here.
+WORDS_ON_TITLE = 3
+WORDS_ON_TEXT = 10
+
+
+class Categories:
+    """Модель для категорий."""
+
+    name = models.TextField(
+        max_length=256,
+        verbose_name='Имя категории'
+    )
+    slug = models.SlugField(
+        max_length=50,
+        unique=True,
+        verbose_name='Слаг категории'
+    )
+
+    class Meta:
+        """Дополнительная информация о модели Categories."""
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self) -> str:
+        """Возвращает все поля сообщества."""
+        return (
+            truncatewords(self.name, WORDS_ON_TEXT)
+        )
