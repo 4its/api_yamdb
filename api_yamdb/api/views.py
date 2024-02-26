4ef276436828fae364964d9c8e6c467d55d9d440
Sergey Kulbida <kulbida.sergey@outlook.com>
2024-02-26 14:54:43 +0700

    Добавил пермишн на автора-или-чтение

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,13 +6,15 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import (
     filters, viewsets, status, permissions, generics, mixins
 )
-from rest_framework.filters import SearchFilter
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.pagination import PageNumberPagination
 
-from .permissions import AdminOrReadOnly, AdminOnly, CategoryPermission
+from .permissions import (AdminOrReadOnly,
+                          AdminOnly,
+                          CategoryPermission,
+                          IsAuthorOrReadOnly)
 from .filters import GenreCategoryFilter
 from .serializers import (
     CategoriesSerializer,
@@ -193,10 +195,12 @@ class ReviewsViewSet(viewsets.ModelViewSet):
 
     - GET;
     - POST;
+    - PATCH;
     - DELETE.
     """
 
     serializer_class = ReviewsSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def get_title(self):
         """Получает объект произведения."""
