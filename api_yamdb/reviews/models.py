8e7f5dca59101106f0c349e799c67d0f9d03041c
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-02-27 19:46:06 +0300

    Сделал константы

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,36 +1,63 @@
-from django.contrib.auth.models import AbstractUser
+import re
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.template.defaultfilters import truncatechars, truncatewords
 
 
-WORDS_ON_TEXT = 10
+WORDS_ON_TEXT = 10      # TODO Подумать, нужно ли...
+RESERVED_USERNAMES = ('me',)
+STANDARD_FIELD_LENGTH = 150
+NAME_FIELD_LENGTH = 256
+SLUG_FIELD_LENGTH = 50
+EMAIL_FIELD_LENGTH = 254
 
+
+def validate_username(username):
+    """Username validation method"""
+    regex = r'^[\w.@+-]+\Z'
+    if not re.match(regex, username):
+        raise ValidationError(
+            'Использованы некорректные символы.', code='invalid_symbols'
+        )
+    if username.lower() in RESERVED_USERNAMES:
+        raise ValidationError(
+            'Выбранное имя является зарезервированным. Используйте иное',
+            code='reserved_username'
+        )
 
 class User(AbstractUser):
-    USERNAME_LENGTH = AbstractUser._meta.get_field('username').max_length
-    EMAIL_FIELD_LENGTH = 254
 
     class RoleChoice(models.TextChoices):
-        user = 'user', 'User'
-        moderator = 'moderator', 'Moderator'
-        admin = 'admin', 'Admin'
+        user = 'user', 'Пользователь'
+        moderator = 'moderator', 'Модератор'
+        admin = 'admin', 'Администратор'
 
     username = models.CharField(
-        max_length=USERNAME_LENGTH,
+        max_length=STANDARD_FIELD_LENGTH,
         unique=True,
         validators=(RegexValidator(
             r'^[\w.@+-]+\Z',
             message='Имя пользователя содержит недопустимые символы'
         ),)
     )
-
     email = models.EmailField(
         verbose_name='Эл.почта',
         unique=True,
         max_length=EMAIL_FIELD_LENGTH,
     )
-
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=STANDARD_FIELD_LENGTH,
+        blank=True
+    )
+    last_name = models.CharField(
+        verbose_name='Фамилия',
+        max_length=STANDARD_FIELD_LENGTH,
+        blank=True
+    )
     bio = models.TextField(
         verbose_name='О себе',
         blank=True,
@@ -47,7 +74,18 @@ class User(AbstractUser):
         verbose_name = 'Пользователи'
         verbose_name_plural = 'пользователи'
         default_related_name = 'users'
-        ordering = ('id',)
+        ordering = ('username',)
+
+    def clean(self):
+        super().clean()
+        regex = r'^[\w.@+-]+\Z'
+        if not re.match(regex, username):
+            raise ValidationError(
+                ('Username состоит только из латинских букв, цифр и'
+                 ' символов @, ., +, -, _,'),
+                code='invalid_symbols'
+            )
+
 
     def __str__(self):
         chars_on_username = 25
