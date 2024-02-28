74e35c78407f0693e4e186c14f2687bf21e125ae
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-02-28 17:40:14 +0300

    константы берутся из settings

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,19 +1,16 @@
 import re
 
+from django.conf import settings
 from django.core.exceptions import ValidationError
 
 
-RESERVED_USERNAMES = ('me',)
-USERNAME_PATTERN = r'^[\w.@+-]+\Z'
-
-
 def validate_username(username):
-    if username.lower() in RESERVED_USERNAMES:
+    if username.lower() in settings.RESERVED_USERNAMES:
         raise ValidationError(
             'Данное имя пользователя зарезервировано системой',
         )
 
-    if re.search(USERNAME_PATTERN, username) is None:
+    if re.search(settings.USERNAME_PATTERN, username) is None:
         raise ValidationError(
             f'Недопустимые символы в имени пользователя {username}'
         )
