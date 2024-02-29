6c4117d6c102ca91f7a84d88f9f245dcf9293535
Sergey Kulbida <kulbida.sergey@outlook.com>
2024-02-29 13:39:33 +0700

    Улучшил код импорта

--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -21,11 +21,37 @@ class Counter:
 
 class Command(BaseCommand):
     help = 'Импортирует записи из CSV в БД.'
+    counter = Counter()
 
     def handle(self, *args, **options):
 
-        counter = Counter()
+        IMPORT_HANDLERS = {
+            self.import_user,
+            self.import_category,
+            self.import_genre,
+            self.import_title,
+            self.import_review,
+            self.import_comment
+        }
 
+        for handler in IMPORT_HANDLERS:
+            try:
+                handler()
+            except BaseException as e:
+                raise ImportError(
+                    f'Ошибка при импорте '
+                    f'{handler.__name__}: {e}. '
+                    f'Проверьте БД на конфликты и повторите попытку.'
+                )
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f'Загрузка данных окончена, '
+                f'создано строк: {self.counter.get_total()}'
+            )
+        )
+
+    def import_user(self):
         with transaction.atomic():
             with open(FILES_PATH + 'users.csv') as file:
                 for row in DictReader(file):
@@ -39,8 +65,9 @@ class Command(BaseCommand):
                         last_name=row['last_name'],
                     )
                     user.save()
-                    counter.add_value()
+                    self.counter.add_value()
 
+    def import_category(self):
         with transaction.atomic():
             for row in DictReader(open(FILES_PATH + 'category.csv')):
                 category = Category(
@@ -49,8 +76,9 @@ class Command(BaseCommand):
                     slug=row['slug'],
                 )
                 category.save()
-                counter.add_value()
+                self.counter.add_value()
 
+    def import_genre(self):
         with transaction.atomic():
             for row in DictReader(open(FILES_PATH + 'genre.csv')):
                 genre = Genre(
@@ -59,60 +87,62 @@ class Command(BaseCommand):
                     slug=row['slug'],
                 )
                 genre.save()
-                counter.add_value()
+                self.counter.add_value()
 
+    def import_title(self):
         genre_title = defaultdict(list)
         with open(FILES_PATH + 'genre_title.csv',
-                  mode='r', encoding='utf8') as f:
-            for row in DictReader(f):
+                  mode='r', encoding='utf8') as file:
+            for row in DictReader(file):
                 genre_title[row['title_id']].append(row['genre_id'])
 
         with transaction.atomic():
-            for row in DictReader(open(FILES_PATH + 'titles.csv')):
-                title_id = row['id']
+            with open(FILES_PATH + 'titles.csv',
+                      mode='r', encoding='utf8') as file:
+                for row in DictReader(file):
+                    title_id = row['id']
 
-                genres = Genre.objects.filter(id__in=genre_title[title_id])
+                    genres = Genre.objects.filter(id__in=genre_title[title_id])
 
-                title = Title(
-                    id=title_id,
-                    name=row['name'],
-                    year=row['year'],
-                    category=Category.objects.get(id=row['category']),
-                )
+                    title = Title(
+                        id=title_id,
+                        name=row['name'],
+                        year=row['year'],
+                        category=Category.objects.get(id=row['category']),
+                    )
 
-                title.genre.set(genres)
+                    title.genre.set(genres)
 
-                title.save()
-                counter.add_value()
+                    title.save()
+                    self.counter.add_value()
 
+    def import_review(self):
         with transaction.atomic():
-            for row in DictReader(open(FILES_PATH + 'review.csv')):
-                review = Review(
-                    id=row['id'],
-                    title_id=row['title_id'],
-                    text=row['text'],
-                    author=User.objects.get(id=row['author']),
-                    score=row['score'],
-                    pub_date=row['pub_date'],
-                )
-                review.save()
-                counter.add_value()
+            with open(FILES_PATH + 'review.csv',
+                      mode='r', encoding='utf8') as file:
+                for row in DictReader(file):
+                    review = Review(
+                        id=row['id'],
+                        title_id=row['title_id'],
+                        text=row['text'],
+                        author=User.objects.get(id=row['author']),
+                        score=row['score'],
+                        pub_date=row['pub_date'],
+                    )
+                    review.save()
+                    self.counter.add_value()
 
+    def import_comment(self):
         with transaction.atomic():
-            for row in DictReader(open(FILES_PATH + 'comments.csv')):
-                comment = Comment(
-                    id=row['id'],
-                    review_id=row['review_id'],
-                    text=row['text'],
-                    author=User.objects.get(id=row['author']),
-                    pub_date=row['pub_date'],
-                )
-                comment.save()
-                counter.add_value()
-
-        self.stdout.write(
-            self.style.SUCCESS(
-                f'Загрузка данных окончена, '
-                f'создано строк: {counter.get_total()}'
-            )
-        )
+            with open(FILES_PATH + 'comments.csv',
+                      mode='r', encoding='utf8') as file:
+                for row in DictReader(file):
+                    comment = Comment(
+                        id=row['id'],
+                        review_id=row['review_id'],
+                        text=row['text'],
+                        author=User.objects.get(id=row['author']),
+                        pub_date=row['pub_date'],
+                    )
+                    comment.save()
+                    self.counter.add_value()
