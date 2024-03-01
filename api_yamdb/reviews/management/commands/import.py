e5aa4c2da1b524c5b8a3f8598f888a551e31b2c0
Sergey Kulbida <101509005+SergeyKDEV@users.noreply.github.com>
2024-03-01 14:02:40 +0700

    Merge pull request #73
    * Добавил managment-команду для импорта в БД из CSV
    
    * Merge branch 'pre_release' into feature/csv_parsing
    
    * Merge branch 'pre_release' into feature/csv_parsing
    
    * Реализовал функцию импорта в БД из CSV
    
    * Улучшил код импорта
    
    * Merge branch 'pre_release' into feature/csv_parsing
    
    * Merge branch 'pre_release' into feature/csv_parsing
    
    * Merge branch 'pre_release' into feature/csv_parsing
    
    * Переписал команду импорта CSV в БД

--- api_yamdb/reviews/management/commands/import.py
+++ api_yamdb/reviews/management/commands/import.py
@@ -1,149 +1,84 @@
-from collections import defaultdict
-from csv import DictReader
+import csv
+import os
 
 from django.conf import settings
 from django.core.management import BaseCommand
-from django.db import transaction
+from django.db import IntegrityError
 
-from reviews.models import Category, Comment, Genre, Review, Title, User
+from reviews.models import (Category, Comment, Genre, GenreTitle, Review,
+                            Title, User)
 
-FILES_PATH = settings.STATIC_URL.strip('/') + '/data/'
+FILES_PATH = os.path.join(settings.BASE_DIR, 'static/data/')
+
+IMPORT_MODELS = {
+    'category': Category,
+    'genre': Genre,
+    'titles': Title,
+    'genre_title': GenreTitle,
+    'users': User,
+    'review': Review,
+    'comments': Comment,
+}
+
+FIELD_MAPPING = {
+    'category': ('category', Category),
+    'title_id': ('title', Title),
+    'genre_id': ('genre', Genre),
+    'author': ('author', User),
+    'review_id': ('review', Review),
+}
 
 
-class Counter:
-    count = 0
+def open_csv_file(file_name):
+    """Открывает CSV файл."""
+    csv_file = file_name + '.csv'
+    csv_path = os.path.join(FILES_PATH, csv_file)
+    try:
+        with open(csv_path, encoding='utf-8') as file:
+            return list(csv.reader(file))
+    except FileNotFoundError:
+        print(f'Файл {csv_file} не найден.')
+        return None
 
-    def add_value(self):
-        self.count += 1
 
-    def get_total(self):
-        return self.count
+def replace_foreign_values(data_csv):
+    """Заменяет значения в CSV данных."""
+    data_csv_copy = data_csv.copy()
+    for field_key, field_value in data_csv.items():
+        if field_key in FIELD_MAPPING:
+            field_name, model_class = FIELD_MAPPING[field_key]
+            data_csv_copy[field_name] = model_class.objects.get(pk=field_value)
+    return data_csv_copy
+
+
+def load_csv(file_name, model_class):
+    """Загружает CSV файлы."""
+    data = open_csv_file(file_name)
+    if not data:
+        return
+
+    header, rows = data[0], data[1:]
+    for row in rows:
+        data_csv = dict(zip(header, row))
+        data_csv = replace_foreign_values(data_csv)
+        try:
+            model_instance = model_class(**data_csv)
+            model_instance.save()
+        except (ValueError, IntegrityError) as error:
+            print(f'Ошибка в загружаемых данных. {error}.')
+            break
+    print(f'Таблица {model_class.__qualname__} загружена.')
 
 
 class Command(BaseCommand):
+    """Команда для импорта в базу данных."""
+
     help = 'Импортирует записи из CSV в БД.'
-    counter = Counter()
 
     def handle(self, *args, **options):
-
-        IMPORT_HANDLERS = {
-            self.import_user,
-            self.import_category,
-            self.import_genre,
-            self.import_title,
-            self.import_review,
-            self.import_comment
-        }
-
-        for handler in IMPORT_HANDLERS:
-            try:
-                handler()
-            except BaseException as e:
-                raise ImportError(
-                    f'Ошибка при импорте '
-                    f'{handler.__name__}: {e}. '
-                    f'Проверьте БД на конфликты и повторите попытку.'
-                )
+        for file_name, model_class in IMPORT_MODELS.items():
+            print(f'Загрузка таблицы {model_class.__qualname__}')
+            load_csv(file_name, model_class)
 
         self.stdout.write(
-            self.style.SUCCESS(
-                f'Загрузка данных окончена, '
-                f'создано строк: {self.counter.get_total()}'
-            )
-        )
-
-    def import_user(self):
-        with transaction.atomic():
-            with open(FILES_PATH + 'users.csv') as file:
-                for row in DictReader(file):
-                    user = User(
-                        id=row['id'],
-                        username=row['username'],
-                        email=row['email'],
-                        role=row['role'],
-                        bio=row['bio'],
-                        first_name=row['first_name'],
-                        last_name=row['last_name'],
-                    )
-                    user.save()
-                    self.counter.add_value()
-
-    def import_category(self):
-        with transaction.atomic():
-            for row in DictReader(open(FILES_PATH + 'category.csv')):
-                category = Category(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],
-                )
-                category.save()
-                self.counter.add_value()
-
-    def import_genre(self):
-        with transaction.atomic():
-            for row in DictReader(open(FILES_PATH + 'genre.csv')):
-                genre = Genre(
-                    id=row['id'],
-                    name=row['name'],
-                    slug=row['slug'],
-                )
-                genre.save()
-                self.counter.add_value()
-
-    def import_title(self):
-        genre_title = defaultdict(list)
-        with open(FILES_PATH + 'genre_title.csv',
-                  mode='r', encoding='utf8') as file:
-            for row in DictReader(file):
-                genre_title[row['title_id']].append(row['genre_id'])
-
-        with transaction.atomic():
-            with open(FILES_PATH + 'titles.csv',
-                      mode='r', encoding='utf8') as file:
-                for row in DictReader(file):
-                    title_id = row['id']
-
-                    genres = Genre.objects.filter(id__in=genre_title[title_id])
-
-                    title = Title(
-                        id=title_id,
-                        name=row['name'],
-                        year=row['year'],
-                        category=Category.objects.get(id=row['category']),
-                    )
-
-                    title.genre.set(genres)
-
-                    title.save()
-                    self.counter.add_value()
-
-    def import_review(self):
-        with transaction.atomic():
-            with open(FILES_PATH + 'review.csv',
-                      mode='r', encoding='utf8') as file:
-                for row in DictReader(file):
-                    review = Review(
-                        id=row['id'],
-                        title_id=row['title_id'],
-                        text=row['text'],
-                        author=User.objects.get(id=row['author']),
-                        score=row['score'],
-                        pub_date=row['pub_date'],
-                    )
-                    review.save()
-                    self.counter.add_value()
-
-    def import_comment(self):
-        with transaction.atomic():
-            with open(FILES_PATH + 'comments.csv',
-                      mode='r', encoding='utf8') as file:
-                for row in DictReader(file):
-                    comment = Comment(
-                        id=row['id'],
-                        review_id=row['review_id'],
-                        text=row['text'],
-                        author=User.objects.get(id=row['author']),
-                        pub_date=row['pub_date'],
-                    )
-                    comment.save()
-                    self.counter.add_value()
+            self.style.SUCCESS('Данные успешно загружены!'))
