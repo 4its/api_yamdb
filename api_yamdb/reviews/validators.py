d8a1b3e0e5d36f5a0d22ce2b977550b4b3872dc4
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-03-04 14:06:24 +0300

    решен вопрос с регуляркой;

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -6,15 +6,16 @@ from django.core.exceptions import ValidationError
 
 
 def validate_username(username):
+    # .lower() применено для исключения вариаций, типа: Me, mE, ME etc...
     if username.lower() in settings.RESERVED_USERNAMES:
         raise ValidationError(
             f'Имя пользователя "{username}" зарезервировано системой'
         )
-    forbidden_chars = [char for char in set(username)
-                       if (re.search(settings.USERNAME_PATTERN, char) is None)]
+    forbidden_chars = re.findall(settings.USERNAME_PATTERN, username)
     if forbidden_chars:
         raise ValidationError(
-            f'Недопустимые символы в имени пользователя: {forbidden_chars}'
+            'Недопустимые символы в'
+            ' имени пользователя: {}'.format(*set(forbidden_chars))
         )
 
 
